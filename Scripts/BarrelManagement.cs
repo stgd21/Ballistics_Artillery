c036fb45eee79430bae5c227c5e3e5c3b0594c79
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-02-29 14:51:15 -0700

    Add files via upload

---
+++ Scripts/BarrelManagement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelManagement : MonoBehaviour
+{
+    public AudioSource stopAudio;
+    public AudioSource movingAudio;
+    public Projectile projectile;
+    bool playedOnce = false;
+    bool allowFiring = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingAudio.isPlaying == false && stopAudio.isPlaying == false && playedOnce == false)
+        {
+            stopAudio.Play();
+            playedOnce = true;
+            Invoke("AllowFiring", 1f);
+        }
+
+        if (allowFiring == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                projectile.enabled = true;
+            }
+        }
+    }
+
+    void AllowFiring()
+    {
+        allowFiring = true;
+    }
+}
---
+++ Scripts/Projectile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Projectile : MonoBehaviour
+{
+    public float myTimeScale = 1.0f;
+    public GameObject target;
+    public float launchForce = 10f;
+    public LookAtTarget cameraControl;
+    public AudioSource fireAudio;
+    public AudioSource hitAudio;
+    public AudioSource windAudio;
+    public Canvas EndGameCanvas;
+
+    Rigidbody rb;
+    bool playedOnce = false;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Time.timeScale = myTimeScale; // allow for slowing time to see what's happening
+        rb = GetComponent<Rigidbody>();
+
+        //Disable animation convenience settings on rigidbody
+        rb.useGravity = true;
+        rb.isKinematic = false;
+
+        FiringSolution fs = new FiringSolution();
+        Nullable<Vector3> aimVector = fs.Calculate(transform.position, target.transform.position, launchForce, Physics.gravity);
+        if (aimVector.HasValue)
+        {
+            rb.AddForce(aimVector.Value.normalized * launchForce, ForceMode.VelocityChange);
+        }
+        fireAudio.Play();
+        Invoke("ActivateCamera", 0.2f);
+        
+    }
+
+    void ActivateCamera()
+    {
+        cameraControl.enabled = true;
+        windAudio.Play();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) < 50 && hitAudio.isPlaying == false && playedOnce == false)
+        {
+            hitAudio.Play();
+            playedOnce = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Collision");
+        EndGameCanvas.gameObject.SetActive(true);
+    }
+}
